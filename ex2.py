ea1d0601cca739dbf9795db62ed9b3b2e5781624
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-09-16 03:00:43 +0700

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,13 @@
+a = input()
 
+count = 1
+maxc = 1
+
+for i in range(len(a) - 1):
+    if a[i] == a[i + 1]:
+        count+=1
+        if count > maxc:
+            maxc = count
+    else:
+        count = 1
+print(maxc)
