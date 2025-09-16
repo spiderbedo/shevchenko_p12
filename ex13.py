043515ce29c51f35b8d92a74e1ea7c9ba1e3fb50
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-09-16 08:36:57 +0700

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -1,1 +1,13 @@
-
+count = 0
+while True:
+    ticket = input().strip()
+    count += 1
+    dig = [int(i) for i in ticket]
+    a = len(dig)
+    if a % 2 == 0:
+        half = n // 2
+        first = sum(dig[:half])
+        sec = sum(dig[half:])
+        if first == sec:
+            print(count)
+            break
