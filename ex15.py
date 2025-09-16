0f5e6fd365f25341a1f98ebea55dd6eaa3e19363
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-09-16 21:56:29 +0700

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -1,1 +1,13 @@
-
+numb = input()
+for i in range(25): 
+    print()
+for j in range(10):
+    attempt = input()
+    bull = sum(1 for k in range(4) if attempt[k] == numb[k])
+    cow = sum(1 for n in attempt if n in numb) - bull
+    print("Быков:", bull, "Коров:", cow)
+    if bull == 4:
+        print("Победа!")
+        break
+else:
+    print("Проигрыш!")
