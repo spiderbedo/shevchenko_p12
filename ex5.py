d6399ad15c0be5e81104e866b91f1577d0841bcf
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-09-16 08:18:18 +0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,1 +1,11 @@
+s1 = input()
+s2 = input()
+s3 = input()
+
+set1 = set(s1)
+set2 = set(s2)
+set3 = set(s3)
+
+usym = (set1 - set2 - set3) | (set2 - set1 - set3) | (set3 - set1 - set2)
+print("".join(usym))
 
