137f4f1a25ff304c44d8266e68f42dad7433a0ad
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-09-16 08:30:20 +0700

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,1 +1,25 @@
+text = input().split()
+cities = set()
 
+for i in range(len(text)):
+    city = text[i].lower()
+    if city in cities:
+        if i % 2 == 0:
+            print("Вася")
+        else:
+            print("Петя")
+        exit()
+    if i > 0:
+        p_last = text[i-1][-1].lower()
+        c_first = text[i][0].lower()
+        if p_last != c_first:
+            if i % 2 == 0:
+                print("Вася")
+            else:
+                print("Петя")
+            exit()
+    cities.add(city)
+if n % 2 == 1:
+    print("Петя")
+else:
+    print("Вася")
